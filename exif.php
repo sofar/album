db2c60ec67bea997cb24a07f77c27cc32c8f383a
Auke Kok <auke@foo-projects.org>
2012-07-31 23:20:14 -0700

    Add exif data loader. Not sure where I'll add this information.

---
+++ exif.php
@@ -0,0 +1,51 @@
+<?
+
+# exif.php
+#
+# Get exif info for an image, and render a XML content that can be inserted
+# into a webpage dynamically
+
+$fields = array (
+	"FILE.FileName" => "Filename",
+	"EXIF.ExifImageWidth" => "Width",
+	"EXIF.ExifImageLength" => "Height",
+	"EXIF.ExposureTime" => "Exposure",
+	"EXIF.FNumber" => "Aperture",
+	"EXIF.DateTimeOriginal" => "Date",
+	"EXIF.ISOSpeedRatings" => "ISO",
+	"EXIF.Flash" => "Flash",
+	"EXIF.FocalLength" => "Focal length",
+	"IFD0.Make" => "Camera make",
+	"IFD0.Model" => "Camera model",
+	"IFD0.DateTime" => "Date",
+	"EXIF.UserComment" => "Comment",
+	"COMPUTED.UserComment" => "Comment"
+);
+$output = array ();
+
+$image = $_GET['i'];
+$user = $_GET['u'];
+
+$album = dirname($image);
+
+$obj = "/home/" . $user . "/album/" . $image;
+
+$exif = exif_read_data($obj, 0, true);
+if (is_array($exif)) {
+	echo "<pre>\n";
+	foreach ($fields as $field => $name) {
+		list ($f1, $f2) = explode(".", $field);
+		if (isset($exif[$f1])) {
+			if (isset($exif[$f1][$f2])) {
+				$output[$name] = $exif[$f1][$f2];
+			}
+		}
+	}
+	foreach ($output as $key => $val)
+		echo $key . ": " . $val . "\n";
+	echo "</pre>\n";
+} else {
+	echo "<p>The image contains no usable EXIV metadata</p>\n";
+}
+
+?>
